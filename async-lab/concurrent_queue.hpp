a8fc2d10fbb215e8ac14f2ccd0bcf95c2e1a3438
Krystian Piekos <krystian.piekos@infotraining.pl>
2026-03-25 12:53:57 +0100

    ConcurrentQueue

---
+++ async-lab/concurrent_queue.hpp
@@ -0,0 +1,92 @@
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+enum class QueueErrc {
+    Success,
+    Empty,
+    Closed,
+    Busy
+};
+
+template <typename T>
+class ConcurrentQueue
+{
+    std::queue<T> q_;
+    mutable std::mutex mtx_q_;
+    std::condition_variable cv_q_not_empty_;
+	std::atomic<bool> is_closed_{false};
+
+public:
+    ConcurrentQueue() = default;
+
+	bool empty() const
+	{
+		std::lock_guard lk{mtx_q_};
+		return q_.empty();
+	}
+
+    bool push(const T& value)
+    {
+		if (is_closed_)
+			return false;
+
+        {
+            std::lock_guard lk{mtx_q_};
+            q_.push(value);
+        }
+        cv_q_not_empty_.notify_one();
+
+		return true;
+    }
+
+    [[nodiscard]] std::optional<T> pop()
+    {
+        std::unique_lock lk{mtx_q_};
+
+		auto open_and_not_empty = [this] { return !q_.empty() || is_closed_; };
+        cv_q_not_empty_.wait(lk, open_and_not_empty);
+
+		if (q_.empty() && is_closed_)
+			return std::nullopt;
+
+        T value = std::move(q_.front());
+        q_.pop();
+        return value;
+    }
+
+	QueueErrc try_pop(T& value)
+	{
+		std::unique_lock lk{mtx_q_, std::try_to_lock};
+
+		if (not lk.owns_lock())
+			return QueueErrc::Busy;
+
+		if (is_closed_)
+			return QueueErrc::Closed;
+
+		if (q_.empty())
+			return QueueErrc::Empty;
+
+		value = std::move(q_.front());
+		q_.pop();
+
+		return QueueErrc::Success;
+	}
+
+	void close()
+	{
+		{
+			std::lock_guard lk{mtx_q_};
+			is_closed_ = true;
+		}
+
+		cv_q_not_empty_.notify_all();
+	}
+
+	bool is_closed() const
+	{
+		return is_closed_;
+	}
+};
--- async-lab/concurrent_queue_tests.cpp
+++ async-lab/concurrent_queue_tests.cpp
@@ -1,13 +1,20 @@
-﻿#include <catch2/catch_test_macros.hpp>
-
-#include <string>
-#include <vector>
+﻿#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <condition_variable>
+#include <iostream>
 #include <list>
 #include <map>
-#include <set>
-#include <iostream>
-#include <algorithm>
 #include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#include <chrono>
+
+#include "concurrent_queue.hpp"
+
+using namespace std::literals;
 
 TEST_CASE("ConcurrentQueue - basic operations")
 {
@@ -32,5 +39,130 @@ TEST_CASE("ConcurrentQueue - basic operations")
         REQUIRE(value.has_value());
         REQUIRE(value.value() == 3);
     }
+
+    SECTION("try_pop")
+    {
+        queue.push(10);
+        int value;
+
+        QueueErrc errc = queue.try_pop(value);
+        REQUIRE(errc == QueueErrc::Success);
+        REQUIRE(value == 10);
+
+        errc = queue.try_pop(value);
+        REQUIRE(errc == QueueErrc::Empty);
+    }
+
+	SECTION("empty")
+    {
+        REQUIRE(queue.empty());
+
+        queue.push(42);
+        REQUIRE_FALSE(queue.empty());
+
+        int value;
+        std::optional<int> opt_value = queue.pop();
+        REQUIRE(queue.empty());
+    }
+
+	SECTION("close")
+    {
+        SECTION("push after close")
+        {
+            queue.close();
+            bool result = queue.push(100);
+            REQUIRE_FALSE(result);
+        }
+
+        SECTION("try_pop after close")
+        {
+            queue.close();
+            int value;
+            QueueErrc errc = queue.try_pop(value);
+            REQUIRE(errc == QueueErrc::Closed);
+        }
+
+        SECTION("pop after close")
+        {
+            queue.close();
+            std::optional<int> value = queue.pop();
+            REQUIRE_FALSE(value.has_value());
+        }
+
+        SECTION("close on non-empty queue")
+        {
+            queue.push(1);
+            queue.push(2);
+            queue.close();
+
+            std::optional<int> value = queue.pop();
+            REQUIRE(value.has_value());
+            REQUIRE(value.value() == 1);
+
+            value = queue.pop();
+            REQUIRE(value.has_value());
+            REQUIRE(value.value() == 2);
+
+            value = queue.pop();
+            REQUIRE_FALSE(value.has_value());
+
+            QueueErrc errc = queue.try_pop(value.emplace());
+            REQUIRE(errc == QueueErrc::Closed);
+        }
+
+		SECTION("all waiting threads are unblocked on close")
+		{
+			std::thread t1([&queue] {
+				std::optional<int> value = queue.pop();
+				REQUIRE_FALSE(value.has_value());
+			});
+
+			std::thread t2([&queue] {
+				std::optional<int> value = queue.pop();
+				REQUIRE_FALSE(value.has_value());
+			});
+
+			std::this_thread::sleep_for(200ms);
+			queue.close();
+
+			t1.join();
+			t2.join();
+		}
+    }
 }
 
+TEST_CASE("ConcurrentQueue - close in multithreading")
+{
+    ConcurrentQueue<int> queue;
+
+    std::atomic<int> items_popped{0};
+
+    std::thread producer([&queue] {
+		for (int i = 0; i < 50; ++i) {
+			queue.push(i);
+			std::this_thread::sleep_for(5ms);
+		} 
+
+		queue.close();
+	});
+
+    std::thread consumer([&queue, &items_popped] {
+        while (true)
+        {
+            std::this_thread::sleep_for(10ms);
+            std::optional<int> value = queue.pop();
+            if (!value.has_value())
+            {
+                break;
+            }
+            ++items_popped;
+        }
+    });
+
+    producer.join();
+    consumer.join();
+
+    REQUIRE(items_popped == 50);
+	REQUIRE(queue.empty());
+	REQUIRE(queue.is_closed());
+}
