ad5da79bc6d42766c25754c84a8bd20db170684e
Krystian Piekos <krystian.piekos@infotraining.pl>
2026-03-26 11:11:51 +0100

    Refactor TaskResumer class to support multiple versions and enhance coroutine functionality

--- coroutines/basic_coroutine_tests.cpp
+++ coroutines/basic_coroutine_tests.cpp
@@ -8,86 +8,203 @@
 #include <numeric>
 #include <set>
 #include <string>
-#include <vector>
-#include <coroutine>
 #include <utility>
+#include <vector>
 
-class TaskResumer
+namespace AsyncLab
 {
-public:
-    struct Promise
-    {   
-        Promise()
-        {
-            std::cout << "Promise()" << std::endl;
-        }
-
-        ~Promise()
-        {
-            std::cout << "~Promise()" << std::endl;
-        }
-
-        TaskResumer get_return_object()
-        {
-            return TaskResumer(std::coroutine_handle<Promise>::from_promise(*this));
-        }
-
-        std::suspend_always initial_suspend() // lazy coroutine on start
-        {
-            std::cout << "+ initial_suspend() suspends coroutine..." << std::endl;
-            return {}; 
-        }
-
-        std::suspend_always final_suspend() noexcept
-        {
-            std::cout << "+ final_suspend() suspends..." << std::endl;
-            return {};
-        }
-
-        void unhandled_exception() 
-        {
-            std::cout << "+ unhandled_exception()" << std::endl;
-            std::terminate();
-        }
-
-        void return_void()
-        {
-            std::cout << "+ Completing coroutine with co_return..." << std::endl;
-        }
-    };   
-
-    TaskResumer(std::coroutine_handle<Promise> handle)
-        : handle_(handle)
-    {}
-
-    TaskResumer(const TaskResumer&) = delete;
-    TaskResumer& operator=(const TaskResumer&) = delete;
-
-    TaskResumer(TaskResumer&& source) noexcept 
-        : handle_{std::exchange(source.handle_, nullptr)}
-    {}    
-
-    ~TaskResumer()
+    namespace ver_1
     {
-        if (handle_)
-            handle_.destroy();
-    }
+        class TaskResumer
+        {
+        public:
+            struct Promise
+            {
+                Promise()
+                {
+                    std::cout << "Promise()" << std::endl;
+                }
 
-    bool resume() 
+                ~Promise()
+                {
+                    std::cout << "~Promise()" << std::endl;
+                }
+
+                TaskResumer get_return_object()
+                {
+                    return TaskResumer(std::coroutine_handle<Promise>::from_promise(*this));
+                }
+
+                std::suspend_always initial_suspend() // lazy coroutine on start
+                {
+                    std::cout << "+ initial_suspend() suspends coroutine..." << std::endl;
+                    return {};
+                }
+
+                std::suspend_always final_suspend() noexcept
+                {
+                    std::cout << "+ final_suspend() suspends..." << std::endl;
+                    return {};
+                }
+
+                void unhandled_exception()
+                {
+                    std::cout << "+ unhandled_exception()" << std::endl;
+                    std::terminate();
+                }
+
+                void return_void()
+                {
+                    std::cout << "+ Completing coroutine with co_return..." << std::endl;
+                }
+            };
+
+            TaskResumer(std::coroutine_handle<Promise> handle)
+                : handle_(handle)
+            { }
+
+            TaskResumer(const TaskResumer&) = delete;
+            TaskResumer& operator=(const TaskResumer&) = delete;
+
+            TaskResumer(TaskResumer&& source) noexcept
+                : handle_{std::exchange(source.handle_, nullptr)}
+            { }
+
+            ~TaskResumer()
+            {
+                if (handle_)
+                    handle_.destroy();
+            }
+
+            bool resume()
+            {
+                if (!handle_ || handle_.done())
+                    return false;
+
+                handle_.resume(); // resuming suspended coroutine
+                return true;
+            }
+
+            using promise_type = Promise;
+
+        private:
+            std::coroutine_handle<Promise> handle_;
+        };
+    } // namespace ver_1
+
+    inline namespace ver_2
     {
-        if (!handle_ || handle_.done())
-            return false;
-        
-        handle_.resume(); // resuming suspended coroutine
-        return true;
-    }
+        template <typename T>
+        struct PromiseBase
+        {
+            std::optional<T> result_;
 
-    using promise_type = Promise;
-private:
-    std::coroutine_handle<Promise> handle_;
-};
+            void return_value(T value)
+            {
+                std::cout << "+ Completing coroutine with co_return with value " 
+                          << value << "..." << std::endl;
+                result_ = std::move(value);
+            }
 
-TaskResumer simple_coroutine(std::string name)
+            T&& result() noexcept
+            {
+                return std::move(*result_);
+            }
+        };
+
+        template <>
+        struct PromiseBase<void>
+        {
+            void return_void()
+            {
+                std::cout << "+ Completing coroutine with co_return..." << std::endl;
+            }
+        };
+
+        template <typename T>
+        class TaskResumer
+        {
+        public:
+            struct Promise : PromiseBase<T>
+            {
+                Promise()
+                {
+                    std::cout << "Promise()" << std::endl;
+                }
+
+                ~Promise()
+                {
+                    std::cout << "~Promise()" << std::endl;
+                }
+
+                TaskResumer get_return_object()
+                {
+                    return TaskResumer(std::coroutine_handle<Promise>::from_promise(*this));
+                }
+
+                std::suspend_always initial_suspend() // lazy coroutine on start
+                {
+                    std::cout << "+ initial_suspend() suspends coroutine..." << std::endl;
+                    return {};
+                }
+
+                std::suspend_always final_suspend() noexcept
+                {
+                    std::cout << "+ final_suspend() suspends..." << std::endl;
+                    return {};
+                }
+
+                void unhandled_exception()
+                {
+                    std::cout << "+ unhandled_exception()" << std::endl;
+                    std::terminate();
+                }
+            };
+
+            TaskResumer(std::coroutine_handle<Promise> handle)
+                : handle_(handle)
+            { }
+
+            TaskResumer(const TaskResumer&) = delete;
+            TaskResumer& operator=(const TaskResumer&) = delete;
+
+            TaskResumer(TaskResumer&& source) noexcept
+                : handle_{std::exchange(source.handle_, nullptr)}
+            { }
+
+            ~TaskResumer()
+            {
+                if (handle_)
+                    handle_.destroy();
+            }
+
+            bool resume()
+            {
+                if (!handle_ || handle_.done())
+                    return false;
+
+                handle_.resume(); // resuming suspended coroutine
+                return true;
+            }
+
+            T result()
+            {
+
+                if constexpr(!std::is_void_v<T>)
+                    return handle_.promise().result();
+                else
+                    return;
+            }
+
+            using promise_type = Promise;
+
+        private:
+            std::coroutine_handle<Promise> handle_;
+        };
+    } // namespace ver_2
+} // namespace AsyncLab
+
+AsyncLab::TaskResumer<void> simple_coroutine(std::string name)
 {
     std::cout << "Starting " << name << "\n";
 
@@ -104,14 +221,37 @@ TaskResumer simple_coroutine(std::string name)
     co_return;
 }
 
+AsyncLab::TaskResumer<int> value_coroutine(std::string name)
+{
+    std::cout << "Starting " << name << "\n";
+
+    co_await std::suspend_always(); // suspension point
+
+    std::cout << name << " has been resumed!" << "\n";
+
+    co_await std::suspend_always(); // suspension point
+
+    std::cout << name << " has been resumed again!" << "\n";
+
+    std::cout << name << " has finished!" << "\n";
+
+    co_return 42;
+}
+
 TEST_CASE("Simple coroutine")
 {
-    TaskResumer coro_1 = simple_coroutine("Coroutine#1");
+    using namespace AsyncLab;
+
+    TaskResumer<void> coro_1 = simple_coroutine("Coroutine#1");
+    TaskResumer<int> coro_2 = value_coroutine("Coroutine#2");
 
     std::cout << "Coroutine#1 created..." << std::endl;
+    std::cout << "Coroutine#2 created..." << std::endl;
 
     do
     {
         std::cout << "Resuming coroutine from main..." << std::endl;
-    } while (coro_1.resume());
-}   
+    } while (coro_1.resume() || coro_2.resume());
+
+    std::cout << "Value from coro_2: " << coro_2.result() << std::endl;
+}
